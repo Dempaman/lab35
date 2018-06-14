40cc9d8501cf971dbbfac0e80f3741776a79a442
Oskar Jonsson <oskarj97@hotmail.com>
2018-06-14 22:18:11 +0200

    edded EditList component

---
+++ src/editList.js
@@ -0,0 +1,70 @@
+/*Alternativ 2: redigerbar lista
+Komponenten ska innehålla ett textfält där användaren kan skriva in en text. Där ska finnas en button,
+som man kan klicka på för att lägga till texten i en lista. Komponenten ska visa listan på något lämpligt sätt.
+När listan renderas ska varje element i den förses med en button som kan användas för att ta bort elementet.
+
+1. Textfält där användaren ska kunna skriva in en text. DONE
+2. Ska finnas en knapp vid textfältet för att lägga till texten i en lista. DONE
+3. Komponenten ska visa listan på lämpligt vis.
+4. När listan renderas ska varje element i den förses med en button som kan användas för att ta bort elementet.
+*/
+
+
+
+
+import React from 'react';
+
+class EditList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      newItem: "",
+      itemList: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+  }
+
+  removeItem(e){
+  let array = this.state.itemList;
+  var i = array.indexOf(e);
+    if(i !== -1) {
+      array.splice(i, 1);
+    }
+  this.setState({itemList: array })
+  }
+
+  addItem(){
+    this.setState({ itemList: [...this.state.itemList, this.state.newItem] })
+    console.log(this.state.itemList)
+  }
+
+  handleChange(e){
+    this.setState({newItem: e.target.value});
+    console.log(this.state.newItem)
+  }
+  render(){
+    var self = this;
+    const dataList = this.state.itemList.map(function(data, index) {
+      return (
+        <li key={index}>
+        <p>{data}</p>
+        <button className="removeButton" onClick={()=> self.removeItem(data)}>Remove</button>
+        </li>
+      )
+    })
+    return(
+      <div>
+        <input className="inputNewItem" onChange={this.handleChange}></input>
+        <button className="submitButton" onClick={this.addItem}>ADD ITEM</button>
+        <ul className="itemList">
+          {dataList}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+export default EditList;
---
+++ src/editList.test.js
@@ -0,0 +1,71 @@
+/*Alternativ 2: redigerbar lista
+Komponenten ska innehålla ett textfält där användaren kan skriva in en text. Där ska finnas en button,
+som man kan klicka på för att lägga till texten i en lista. Komponenten ska visa listan på något lämpligt sätt.
+När listan renderas ska varje element i den förses med en button som kan användas för att ta bort elementet.
+
+1. Textfält där användaren ska kunna skriva in en text.
+2. Ska finnas en knapp vid textfältet för att lägga till texten i en lista.
+3. Komponenten ska visa listan på lämpligt vis.
+4. När listan renderas ska varje element i den förses med en button som kan användas för att ta bort elementet.
+*/
+
+
+import React from 'react';
+import {shallow} from 'enzyme';
+import EditList from './editList.js';
+
+
+describe('EditList', () => {
+  it('#1 shallow smoke test <EditList /> ', () => {
+    let wrapper = shallow(<EditList />);
+  });
+  it('#2 renders input', () => {
+    let input = shallow(<input className="inputNewItem"></input>);
+    const inputNewItem = input.is('.inputNewItem')
+    expect(inputNewItem).toBe(true);
+  });
+  it('#3 renders button textfield ', () => {
+    let button = shallow(<button className='submitButton'>ADD ITEM</button>);
+    const submitButton = button.is('.submitButton')
+    expect(submitButton).toBe(true);
+  });
+  it('#4 renders list ', () => {
+    let list = shallow(<ul className="itemList"></ul>);
+    const listOfItem = list.is('.itemList')
+    expect(listOfItem).toBe(true);
+  });
+  it('#5 renders remove button ', () => {
+    let wrapper = shallow(<EditList />);
+
+    let input = wrapper.find("input").at(0);
+    let button = wrapper.find("button").at(0);
+    let list = wrapper.find("li").at(0);
+
+    input.simulate("change", {target: {value: "Milk"}});
+    button.simulate("click");
+
+    let removeBtn = wrapper.find("button.removeButton").length
+    expect(removeBtn).toBe(1);
+  });
+  it('#6 new list item ', () => {
+    let wrapper = shallow(<EditList />);
+
+    let input = wrapper.find("input").at(0);
+		let button = wrapper.find("button").at(0);
+
+    input.simulate("change", {target: {value: "Milk"}});
+		button.simulate("click");
+
+		const shouldOutput = <p>Milk</p>;
+		expect(wrapper.contains(shouldOutput)).toBe(true);
+  });
+  it('#7 remove list item ', () => {
+    let wrapper = shallow(<EditList />);
+
+    wrapper.setState({itemList: ["Milk", "Egg", "Butter"]})
+
+    let removeButton = wrapper.find("button").at(1);
+		removeButton.simulate("click");
+		expect(wrapper.state("itemList").length).toBe(2)
+  });
+})
