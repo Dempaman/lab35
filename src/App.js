eb6d7b202814ba16097dbbdedad72c31853a142f
Dennis Björk <dennis.bjork87@gmail.com>
2018-06-15 13:22:13 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Counter from './counter.js'
 import TextReverese from './text.js'
+import EditList from './editList.js';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Counter/>
         <TextReverese />
+        <EditList />
       </div>
     );
   }
